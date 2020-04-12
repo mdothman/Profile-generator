a8e2baa099dff5ee369a61d060af5e6c8d0795a7
Rooster <othmanmuhidean@gmail.com>
2020-04-11 20:52:11 -0700

    After receiving user input I am able to retrieve response.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,36 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const HTML = require("./generatedHTML");
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const questions = [
-    {
-        type: 'input',
-        name: 'color',
-        message: "What's your favorite color ?",
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'What is your Github username?'
-    }
+  {
+    type: "input",
+    name: "color",
+    message: "What's your favorite color ?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your Github username?",
+  },
 ];
 
-inquirer.prompt(questions).then(answers => {
-  
-} );
+inquirer.prompt(questions).then((answers) => {
+  let color = answers.color;
+  let user = answers.github;
 
-function writeToFile(fileName, data) {
- 
-}
+  const queryUrl = `https://api.github.com/users/${user}`;
 
-function init() {
+  axios.get(queryUrl).then(function (res) {
+    console.log(res);
+  });
+});
+
+function writeToFile(fileName, data) {}
+
+function init() {}
 
 init();
